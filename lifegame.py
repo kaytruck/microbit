a8506c1b23572f28e6bb941b4c4e06022564215a
kaytruck <kayta.sino@gmail.com>
2017-12-08 00:20:14 +0900

    bool配列はやめて、文字列で状態を持つように変更

--- lifegame.py
+++ lifegame.py
@@ -1,75 +1,82 @@
-# Add your Python code here. E.g.
 from microbit import *
 
-current_data = [
-        False, True,  False,  False, False,
-        False, False, True,   True,  False,
-        True,  True,  False,  False, False,
-        False, False, True,   False, False,
-        False, False, False,  False, False,
-        ]
+# initialization data
+init_data = "01000" \
+            "00110" \
+            "11000" \
+            "00100" \
+            "00000"
 
-def draw(bool_array):
+#current_data = [
+#        False, True,  False,  False, False,
+#        False, False, True,   True,  False,
+#        True,  True,  False,  False, False,
+#        False, False, True,   False, False,
+#        False, False, False,  False, False,
+#        ]
+
+def draw(data_str):
     canvas_array = []
-    for i in range(0, len(bool_array)):
-        if bool_array[i]:
+    for i in range(0, len(data_str)):
+        if data_str[i] == '1':
             canvas_array.append('5')
         else:
             canvas_array.append('0')
-        if i % 5 == 4 and i != len(bool_array) - 1:
+        if i % 5 == 4 and i != len(data_str) - 1:
+            # insert ':' into every end of line
             canvas_array.append(':')
 
     canvas_str = ''.join(canvas_array)
-    #print(canvas_str)
-    canvas = Image(canvas_str)
-    display.show(canvas)
+    image = Image(canvas_str)
+    display.show(image)
     
 def next_age(current_data):
     next_data = []
     for i in range(0, len(current_data)):
+        # check around cell
         neighbor = 0
         if i % 5 != 0:
             # left
-            if current_data[i - 1]:
+            if current_data[i - 1] == '1':
                 neighbor += 1
             # up left
-            if i > 4 and current_data[i - 6]:
+            if i > 4 and current_data[i - 6] == '1':
                 neighbor += 1
             # down left
-            if len(current_data) - i > 4 and current_data[i + 4]:
+            if len(current_data) - i > 4 and current_data[i + 4] == '1':
                 neighbor += 1
         if i > 4:
             # up
-            if current_data[i - 5]:
+            if current_data[i - 5] == '1':
                 neighbor += 1
         if i % 5 != 4:
             # right
-            if current_data[i + 1]:
+            if current_data[i + 1] == '1':
                 neighbor += 1
             # up right
-            if i > 4 and current_data[i - 4]:
+            if i > 4 and current_data[i - 4] == '1':
                 neighbor += 1
             # down right
-            if len(current_data) - i > 7 and current_data[i + 6]:
+            if len(current_data) - i > 7 and current_data[i + 6] == '1':
                 neighbor += 1
         if len(current_data) - i > 5:
             # down
-            if current_data[i + 5]:
+            if current_data[i + 5] == '1':
                 neighbor += 1
 
-        if not current_data[i] and neighbor == 3:
-            next_data.append(True)
-        elif current_data[i] and (neighbor == 2 or neighbor == 3):
-            next_data.append(True)
+        # judge next generation 
+        if current_data[i] == '0' and neighbor == 3:
+            next_data.append('1')
+        elif current_data[i] == '1' and (neighbor == 2 or neighbor == 3):
+            next_data.append('1')
+        elif current_data[i] == '1' and (neighbor == 1 or neighbor == 4):
+            next_data.append('0')
         else:
-            next_data.append(False)
-    return next_data
+            next_data.append(current_data[i])
+    return ''.join(next_data)
 
-
+current_data = init_data
 while True:
-    #display.scroll('Hello, World!')
-    #display.show(Image.HEART)
-    #display.show(canvas)
     draw(current_data)
     current_data = next_age(current_data)
     sleep(1000)
